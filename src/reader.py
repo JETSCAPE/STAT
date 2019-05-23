cb20462ba831853dc17a825733c4ce59fa8eea44
Yi Chen <chen.yi.first@gmail.com>
2019-05-23 16:50:54 +0200

    Reader functions to read data files with v1.0 specification

---
+++ src/reader.py
@@ -0,0 +1,147 @@
+import numpy as np
+
+import os
+import pickle
+from pathlib import Path
+
+
+def ReadDesign(FileName):
+    # This is the output object
+    Result = {}
+    Version = ''
+
+    Result["FileName"] = FileName
+
+    # First read all the header information
+    for Line in open(FileName):
+        Items = Line.split()
+        if (len(Items) < 2): continue
+        if Items[0] != '#': continue
+
+        if(Items[1] == 'Version'):
+            Version = Items[2]
+        elif(Items[1] == 'Parameter'):
+            Result["Parameter"] = Items[2:]
+
+    if(Version != '1.0'):
+        raise AssertionError('Bad file version number while reading design points')
+
+    # Then read the actual design parameters
+    Result["Design"] = np.loadtxt(FileName)
+    return Result
+
+def ReadData(FileName):
+    # Initialize objects
+    Result = {}
+    Version = ''
+
+    Result["FileName"] = FileName
+
+    # First read all the header information
+    for Line in open(FileName):
+        Items = Line.split()
+        if (len(Items) < 2): continue
+        if Items[0] != '#': continue
+
+        if(Items[1] == 'Version'):
+            Version = Items[2]
+        elif(Items[1] == 'DOI'):
+            Result["DOI"] = Items[2:]
+        elif(Items[1] == 'Source'):
+            Result["Source"] = Items[2:]
+        elif(Items[1] == 'System'):
+            Result["System"] = Items[2]
+        elif(Items[1] == 'Centrality'):
+            Result["Centrality"] = Items[2:4]
+        elif(Items[1] == 'XY'):
+            Result["XY"] = Items[2:4]
+        elif(Items[1] == 'Label'):
+            Result["Label"] = Items[2:]
+
+    if(Version != '1.0'):
+        raise AssertionError('Bad file version number while reading design points')
+
+    XMode = ''
+    if(Result["Label"][0:4] == ['x', 'y', 'stat,low', 'stat,high']):
+        XMode = 'x'
+    elif(Result["Label"][0:5] == ['xmin', 'xmax', 'y', 'stat,low', 'stat,high']):
+        XMode = 'xminmax'
+    else:
+        raise AssertionError('Invalid list of initial columns!  Should be ("x", "y", "stat,low", "stat,high"), or ("xmin", "xmax", "y", "stat,low", "stat,high")')
+
+    # Then read the actual design parameters
+    RawData = np.loadtxt(FileName)
+
+    Result["Data"] = {}
+    if(XMode == 'x'):
+        Result["Data"]["x"] = RawData[:, 0]
+        Result["Data"]["y"] = RawData[:, 1]
+        Result["Data"]["yerr"] = {}
+        Result["Data"]["yerr"]["stat"] = RawData[:, 2:4]
+        Result["Data"]["yerr"]["sys"] = RawData[:, 4:]
+        Result["SysLabel"] = Result["Label"][4:]
+    elif(XMode == 'xminmax'):
+        Result["Data"]["x"] = (RawData[:, 0] + RawData[:, 1]) / 2
+        Result["Data"]["xerr"] = (RawData[:, 1] - RawData[:, 0]) / 2
+        Result["Data"]["y"] = RawData[:, 2]
+        Result["Data"]["yerr"] = {}
+        Result["Data"]["yerr"]["stat"] = RawData[:, 3:5]
+        Result["Data"]["yerr"]["sys"] = RawData[:, 5:]
+        Result["SysLabel"] = Result["Label"][5:]
+
+    return Result
+
+def ReadCovariance(FileName):
+    # Initialize objects
+    Result = {}
+    Version = ''
+
+    Result["FileName"] = FileName
+
+    # First read all the header information
+    for Line in open(FileName):
+        Items = Line.split()
+        if (len(Items) < 2): continue
+        if Items[0] != '#': continue
+
+        if(Items[1] == 'Version'):
+            Version = Items[2]
+        elif(Items[1] == 'Data1'):
+            Result["Data1"] = Items[2]
+        elif(Items[1] == 'Data2'):
+            Result["Data2"] = Items[2]
+
+    if(Version != '1.0'):
+        raise AssertionError('Bad file version number while reading design points')
+
+    # Then read the actual design parameters
+    Result["Matrix"] = np.loadtxt(FileName)
+    return Result
+
+def ReadPrediction(FileName):
+    # Initialize objects
+    Result = {}
+    Version = ''
+
+    Result["FileName"] = FileName
+
+    # First read all the header information
+    for Line in open(FileName):
+        Items = Line.split()
+        if (len(Items) < 2): continue
+        if Items[0] != '#': continue
+
+        if(Items[1] == 'Version'):
+            Version = Items[2]
+        elif(Items[1] == 'Data'):
+            Result["Data"] = Items[2]
+        elif(Items[1] == 'Design'):
+            Result["Design"] = Items[2]
+
+    if(Version != '1.0'):
+        raise AssertionError('Bad file version number while reading design points')
+
+    # Then read the actual design parameters
+    Result["Prediction"] = np.loadtxt(FileName).T
+    return Result
+
