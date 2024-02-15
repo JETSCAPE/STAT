67862d556358c0ba0f0a8b851d2c503e98c80a26
Cameron Parker <cameron.parker@tamu.edu>
2024-02-15 16:33:46 -0600

    removing one redundancy

--- src/mcmc.py
+++ src/mcmc.py
@@ -33,7 +33,7 @@ import h5py
 import numpy as np
 from scipy.linalg import lapack
 from . import workdir
-from .reader import systems, observables, exp_data_list, exp_cov#, expt
+from .reader import systems, observables, exp_data_list, exp_cov, tuneData
 from .design import Design
 from .emulator import emulators
 import pickle
@@ -297,85 +297,8 @@ class Chain:
 
             yield from klr
         """
-        def getData(picklefile):
-            global AllData
-            AllData = pickle.load((workdir / picklefile).open('rb'))
-
-            #: Sets the collision systems for the entire project,
-            #: where each system is a string of the form
-            #: ``'<projectile 1><projectile 2><beam energy in GeV>'``,
-            #: such as ``'PbPb2760'``, ``'AuAu200'``, ``'pPb5020'``.
-            #: Even if the project uses only a single system,
-            #: this should still be a list of one system string.
-            global systems
-            systems = AllData["systems"]
-
-            #: Design attribute. This is a list of
-            #: strings describing the inputs.
-            #: The default is for the example data.
-            global keys
-            keys = AllData["keys"]
-
-            #: Design attribute. This is a list of input
-            #: labels in LaTeX for plotting.
-            #: The default is for the example data.
-            global labels
-            labels = AllData["labels"]
-
-            #: Design attribute. This is list of tuples of
-            #: (min,max) for each design input.
-            #: The default is for the example data.
-            global ranges
-            ranges = AllData["ranges"]
-
-            #: Design array to use - should be a numpy array.
-            #: Keep at None generate a Latin Hypercube with above (specified) range.
-            #: Design array for example is commented under default.
-            global design_array
-            design_array = AllData["design"]
-
-            #: Dictionary of the model output.
-            #: Form MUST be data_list[system][observable][subobservable][{'Y': ,'x': }].
-            #:     'Y' is an (n x p) numpy array of the output.
-            #:
-            #:     'x' is a (1 x p) numpy array of numeric index of columns of Y (if exists). In the example data, x is p_T.
-            #: This MUST be changed from None - no built-in default exists. Uncomment the line below default for example.
-            global data_list
-            data_list = AllData["model"]
-
-            #: Dictionary for the model validation output
-            #: Must be the same for as the model output dictionary
-            #data_list_val = pickle.load((cachedir / 'model/validation/data_dict_val.p').open('rb'))
-            global data_list_val
-            data_list_val = None
-
-            #: Dictionary of the experimental data.
-            #: Form MUST be exp_data_list[system][observable][subobservable][{'y':,'x':,'yerr':{'stat':,'sys'}}].
-            #:      'y' is a (1 x p) numpy array of experimental data.
-            #:
-            #:      'x' is a (1 x p) numpy array of numeric index of columns of Y (if exists). In the example data, x is p_T.
-            #:
-            #:      'yerr' is a dictionary with keys 'stat' and 'sys'.
-            #:
-            #:      'stat' is a (1 x p) array of statistical errors.
-            #:
-            #:      'sys' is a (1 x p) array of systematic errors.
-            #: This MUST be changed from None - no built-in default exists. Uncomment the line below default for example.
-            global exp_data_list
-            exp_data_list = AllData["data"]
-
-            #: Experimental covariance matrix.
-            #: Set exp_cov = None to have the script estimate the covariance matrix.
-            #: Example commented below default.
-            global exp_cov
-            exp_cov = AllData["cov"]
-
-
-            #: Observables to emulate as a list of 2-tuples
-            #: ``(obs, [list of subobs])``.
-            global observables
-            observables = AllData["observables"]
-
+        
+        """
         def keys_labels_range():
             d = Design(systems[0],labels=labels,ranges=ranges)
             klr = zip(keys, labels, ranges)
@@ -383,8 +306,13 @@ class Chain:
             yield from klr
 
             #yield 'model_sys_err', r'$\sigma\ \mathrm{model\ sys}$', (0., .4)
-        getData(picklefile)
+        #getData(picklefile)
         self.keys, self.labels, self.range = map(list, zip(*keys_labels_range()))
+        """
+        tunedata = tuneData(picklefile)
+        self.keys = tunedata.keys
+        self.labels = tunedata.labels
+        self.range = tunedata.ranges
 
         # print(self.range)
         self.ndim = len(self.range)
@@ -397,7 +325,7 @@ class Chain:
         self._expt_cov = {}
         self.observables = observables
         # pre-compute the experimental data vectors and covariance matrices
-        for sys, sysdata in exp_data_list.items():
+        for sys, sysdata in tunedata.exp_data_list.items():
             nobs = 0
 
             self._slices[sys] = []
